dae9a9eb1bded33ab440eaa2d19b29dfca537b68
Hamidusman <abdulhamidusman218@gmail.com>
2024-05-20 20:58:38 +0100

    link to github

--- weatherdetector/settings.py
+++ weatherdetector/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,13 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret key used in production  secret!
 SECRET_KEY = 'django-insecure-j_%!0kr_ch5s#t@wag41x3s2dune)np)l)vf=cp5(95jz-k&e6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
